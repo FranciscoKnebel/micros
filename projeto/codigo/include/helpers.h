4eabf44de53e442cef9def794d2f8af07aafe3ed
Francisco Knebel <franciscopaivaknebel@gmail.com>
2018-11-30 11:46:19 -0200

    docs

--- projeto/codigo/include/helpers.h
+++ projeto/codigo/include/helpers.h
@@ -1,10 +1,8 @@
 /**
  * @file helpers.h
- * @author AUTHOR NAME
- * @date 25 Nov 2018
+ * @author Francisco Knebel, Luciano Zancan, Rodrigo Dal Ri
+ * @date 30 Nov 2018
  * @brief Helper functions for usage with QuanserShield modules.
- *
- * Detailed.
  */
 
 #include <stdio.h>
--- projeto/codigo/src/utils/helpers.c
+++ projeto/codigo/src/utils/helpers.c
@@ -3,8 +3,6 @@
  * @author Francisco Knebel, Luciano Zancan, Rodrigo Dal Ri
  * @date 30 Nov 2018
  * @brief File containing example of doxygen usage for quick reference.
- *
- * Full description. 	
  */
 
 #include "helpers.h"
